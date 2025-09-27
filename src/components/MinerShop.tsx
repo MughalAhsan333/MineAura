4cabcb3112e52e2a32f8d3c972f1bad25811e463
MughalAhsan333 <mughalahsan5911@gmail.com>
2025-09-27 16:22:30 +0500

    Update MinerShop.tsx

--- src/components/MinerShop.tsx
+++ src/components/MinerShop.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useAccount } from 'wagmi';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../lib/supabase';
 import PaymentModal from './PaymentModal';
 
 interface Miner {
