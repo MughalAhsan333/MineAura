649dfb86237bb00ebfdaba9ed2e572061714f713
MughalAhsan333 <mughalahsan5911@gmail.com>
2025-09-27 16:20:34 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['avatars.githubusercontent.com'], // We might need this later for images
+    domains: ['ujbeatnyhhpqwtwybcqr.supabase.co'],
   },
 }
 
